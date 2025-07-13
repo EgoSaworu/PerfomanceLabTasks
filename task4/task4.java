0546ad5be0aaf1e26dfdf94e0958754e7b166c92
EgoSaworu <133762429+EgoSaworu@users.noreply.github.com>
2025-07-14 00:14:56 +0300

    task4.java

--- task4/task4.java
+++ task4/task4.java
@@ -2,25 +2,25 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 public class task4 {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Необходимо указать путь к файлу в качестве аргумента");
+            return;
+        }
 
-        Scanner scanner = new Scanner(System.in);
-
-        String filePath = scanner.nextLine();
-
+        String filePath = args[0];
         List<Integer> nums = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
-            while ((line = reader.readLine()) != null){
+            while ((line = reader.readLine()) != null) {
                 nums.add(Integer.parseInt(line.trim()));
             }
-        } catch (IOException e){
-            System.err.println("File reading error" + e.getMessage()+ '!');
-        } catch (NumberFormatException e){
+        } catch (IOException e) {
+            System.err.println("File reading error" + e.getMessage() + '!');
+        } catch (NumberFormatException e) {
             System.err.println("The file contains non-numerical values!");
         }
 
@@ -30,14 +30,14 @@ public class task4 {
         }
 
         Collections.sort(nums);
-         int median = 0;
-         int n = nums.size();
-         if (n%2 == 1){
-             median = nums.get(n/2);
-         } else {
-             median = (nums.get(n / 2 - 1) + nums.get(n / 2)) / 2;
-         }
-        int finalMedian = median;
+        int median = 0;
+        int n = nums.size();
+        if (n % 2 == 1) {
+            median = nums.get(n / 2);
+        } else {
+            median = (nums.get(n / 2 - 1) + nums.get(n / 2)) / 2;
+        }
+
         int moves = 0;
         for (int num : nums) {
             moves += Math.abs(num - median);
